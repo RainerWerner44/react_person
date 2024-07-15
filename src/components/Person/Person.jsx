055194bbc6a842b017b15a45d07ef65336768e95
Vadym Babisov <vadim.babisov@gmail.com>
2024-07-15 20:56:21 +0300

    fixed code 2.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,25 +1,28 @@
+const MALE = 'm';
+const FEMALE = 'f';
+
 export const Person = ({ person }) => {
-  const { name, age = undefined, sex, isMarried, partnerName } = person;
+  const { name, age, sex, isMarried, partnerName } = person;
 
-  const ageText =
-    age !== undefined ? <p className="Person__age">I am {age}</p> : null;
-
-  let partnerText;
+  let partnerText = 'I am not married';
 
   if (isMarried) {
-    if (sex === 'm') {
-      partnerText = `${partnerName} is my wife`;
-    } else if (sex === 'f') {
-      partnerText = `${partnerName} is my husband`;
+    switch (sex) {
+      case MALE:
+        partnerText = `${partnerName} is my wife`;
+        break;
+      case FEMALE:
+        partnerText = `${partnerName} is my husband`;
+        break;
+      default:
+        break;
     }
-  } else {
-    partnerText = 'I am not married';
   }
 
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
-      {ageText}
+      {age && <p className="Person__age">I am {age}</p>}
       <p className="Person__partner">{partnerText}</p>
     </section>
   );
